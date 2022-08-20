677fa748295b97732718708952f0d9822cd873e9
seadeef <kevin@drymail.com>
2022-08-19 20:44:49 -0700

    Clean up settings.py

--- settings.py
+++ settings.py
@@ -1,48 +1,35 @@
-import os, subprocess, platform
+from os import environ
 from os.path import join, dirname
-import sys
 from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-## The use of these variables is highly discouraged, as they are not updated if Admin change it
-RECV_EMAIL = os.environ.get("RECV_EMAIL")
-SEND_EMAIL = os.environ.get("SEND_EMAIL")
-EMAIL_PASSWD = os.environ.get("EMAIL_PASSWD")
-ADMIN_PASSWD = os.environ.get("ADMIN_PASSWD")
-ALERT_THRESHOLD = int(os.environ.get("ALERT_THRESHOLD"))
-LOG_PATH = os.environ.get("LOG_PATH")
-TEACHER_NAME = os.environ.get("TEACHER_NAME")
-
-## On the other hand, this dictionary is updated if Admin changes it
 ALL = {
-	"RECV_EMAIL": RECV_EMAIL,
-	"SEND_EMAIL": SEND_EMAIL,
-	"EMAIL_PASSWD": EMAIL_PASSWD,
-	"ADMIN_PASSWD": ADMIN_PASSWD,
-	"TEACHER_NAME": TEACHER_NAME,
-	"ALERT_THRESHOLD": ALERT_THRESHOLD,
-	"LOG_PATH": LOG_PATH,
+	"RECV_EMAIL": environ.get("RECV_EMAIL"),
+	"SEND_EMAIL": environ.get("SEND_EMAIL"),
+	"EMAIL_PASSWD": environ.get("EMAIL_PASSWD"),
+	"ADMIN_PASSWD": environ.get("ADMIN_PASSWD"),
+	"TEACHER_NAME": environ.get("TEACHER_NAME"),
+	"ALERT_THRESHOLD": int(environ.get("ALERT_THRESHOLD")),
+	"LOG_PATH": environ.get("LOG_PATH")
 }
 
 def update(new):
-	global ALL
-
 	# Make sure no errors seep through, just report them
 	try:
 		with open(dotenv_path, 'w') as file:
 			TEMPLATE = f'''
-RECV_EMAIL="{new["RECV_EMAIL"]}"
-SEND_EMAIL="{new["SEND_EMAIL"]}"
-EMAIL_PASSWD="{new["EMAIL_PASSWD"]}"
-ADMIN_PASSWD="{new["ADMIN_PASSWD"]}"
-TEACHER_NAME="{new["TEACHER_NAME"]}"
-ALERT_THRESHOLD={new["ALERT_THRESHOLD"]} # minutes
-LOG_PATH="{new["LOG_PATH"]}"
-FLASK_APP="backend.py"
-FLASK_DEBUG=1
-				'''
+				RECV_EMAIL="{new["RECV_EMAIL"]}"
+				SEND_EMAIL="{new["SEND_EMAIL"]}"
+				EMAIL_PASSWD="{new["EMAIL_PASSWD"]}"
+				ADMIN_PASSWD="{new["ADMIN_PASSWD"]}"
+				TEACHER_NAME="{new["TEACHER_NAME"]}"
+				ALERT_THRESHOLD={new["ALERT_THRESHOLD"]} # minutes
+				LOG_PATH="{new["LOG_PATH"]}"
+				FLASK_APP="backend.py"
+				FLASK_DEBUG=1
+			'''
 
 			file.write(TEMPLATE)
 			
